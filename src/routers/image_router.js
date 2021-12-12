68106ee68375340023c9202f253817dcb5e78eff
Xuan Hung <nxhung16102001@gmail.com>
2021-12-12 16:58:55 +0700

    update - image load

--- src/routers/image_router.js
+++ src/routers/image_router.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const  imageController  = require("../controllers/image_controller");
+const imageController = require("../controllers/image_controller");
 const authMiddleWare = require("../middleware/auth");
-router.get("/:id", authMiddleWare, imageController.getImage);
+router.get("/:id", imageController.getImage);
 
 module.exports = router;
